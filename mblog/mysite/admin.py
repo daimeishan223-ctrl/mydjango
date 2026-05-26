b566e5d6c60bc9c20fa7b58a1c9ec860a3002082
daimeishan223-ctrl <daimeishan223@gmail.com>
2026-05-26 16:37:28 +0800

    upload files

---
+++ mblog/mysite/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from mysite.models import Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+
+admin.site.register(Post, PostAdmin)
